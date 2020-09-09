ae298f1338ec53dfe06e1417b9862b61ea002245
salim ameur <salim.ameur@gmail.com>
2020-09-09 17:44:58 +0200

    img change and work on the css

--- script.js
+++ script.js
@@ -11,9 +11,18 @@ var buttons = document.querySelectorAll('.choose');
 var playerScoreEl= document.getElementById('scoreOne');
 var computerScoreEl= document.getElementById('scoreTwo');
 var pentagone= document.getElementById('pentagone');
-var winnerBox= document.getElementById('winner-box');
+var selectionBox= document.getElementById('selection-box');
 var playAgain= document.getElementById('play-again')
-// Step 1 create a fonction where the computer is picking a rundom choice  (computer choice)
+var playerSelection= document.getElementById('player-selection');
+var computerSelection= document.getElementById('computer-selection');
+var result= document.getElementById('result');
+
+
+// var username = prompt("What is your name?");
+
+// console.log(username);
+
+//  function where the computer is picking a rundom choice  (computer choice)
 
 function pickRondomChoice(){
         return choice[Math.floor(Math.random() * choice.length)]; 
@@ -21,7 +30,11 @@ function pickRondomChoice(){
  //console.log(pickRondomChoice());
 
 
-// Step 2  create a function where the user pick his own choice when he click on it (player choice)
+
+
+
+
+// function where the user pick his own choice when he click on it (player choice)
 
  buttons.forEach((button) => {
        
@@ -29,25 +42,34 @@ function pickRondomChoice(){
    () => {
     playerChoice = button.getAttribute('data-choice');
  
- // hide the pentagone and show the player and computer selection
+ // hide the pentagone and show the selection-box
 
     pentagone.style.display = 'none';
-    winnerBox.style.display = 'flex'
+    selectionBox.style.display = 'flex'
     checkWinner();  
    });
  
 });
 
-// coming back to the pentagone page when the player decide to play again
+
+
+
+
+
+// Going back to the pentagone page when the player decide to play again
 
 playAgain.addEventListener("click",() =>{
 
     pentagone.style.display = 'flex';
-    winnerBox.style.display = 'none';
+    selectionBox.style.display = 'none';
 
 });
 
-// Step 4 update score number function player
+
+
+
+
+//  update score number for the player
 
 
 function updatePlayerScore(value){
@@ -55,7 +77,7 @@ function updatePlayerScore(value){
     playerScoreEl.innerText = playerScore;
 }
 
-// update score number computer 
+// update score number for the computer 
 
 function updateComputerScore(value){
         value = computerScore++;
@@ -65,15 +87,24 @@ function updateComputerScore(value){
 
 
 
-
-
-// step 3 who is the winner function 
+//  the winner function 
 
 function checkWinner (){
         let computerChoice= pickRondomChoice();
+
+
+
+    // updating the selection 
+
+
+    // updateSelectionbox(playerSelection, playerChoice);
+    // updateSelectionbox(computerSelection, computerChoice);
     
         if ( playerChoice === computerChoice){
             // draw
+            result.innerHTML='DRAW';
+
+
         } else if ( (playerChoice === 'robot' && (computerChoice === 'ninja' || computerChoice ==='zombie')) 
         || (playerChoice === 'pirate' && (computerChoice ==='robot' || computerChoice ==='monkey' ))
         || (playerChoice === 'ninja' && (computerChoice === 'pirate' || computerChoice ==='zombie'))
@@ -81,18 +112,35 @@ function checkWinner (){
         || (playerChoice === 'zombie' && (computerChoice === 'monkey'|| computerChoice ==='pirate'))
         ) {
                 updatePlayerScore(1);
+                result.innerHTML='YOU WIN';
         } else {
             // user loose
             updateComputerScore(1)
+            result.innerHTML='YOU LOSE';
         }
         
         console.log(checkWinner);
     }
 
 
-// step 5 update score of the winner 
 
-// STEP 6 end of the game after 3 parti 
+    
+// updating the selection box with the choices
+
+ function updateSelectionbox ( selectionBox , choice ){
+selectionBox.classlist.remove('btn-robot');
+selectionBox.classlist.remove('btn-pirate');
+selectionBox.classlist.remove('btn-ninja');
+selectionBox.classlist.remove('btn-monkey');
+selectionBox.classlist.remove('btn-zombie');
+
+selectionBox.classlist.add(`btn-${choice}`);
+
+const img=selectionBox.querySelector('img');
+img.src = `style/images/icon-${choice}.svg`;
+img.alt= choice;
+ };
+
 
 
 
